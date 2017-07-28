067c72e896b9325c91b1025a930193998cf26893
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-27 23:10:40 -0600

    Compiles, but fails tests.

---
+++ leap/leap.ts
@@ -0,0 +1,14 @@
+// export default class isLeapYear {
+//
+//   constructor() {
+//   }
+//
+// }
+
+// export function isLeapYear(year: number): boolean {
+//   return year % 4 == 0;
+// }
+
+export default function isLeapYear(year: number): boolean {
+  return false;
+}
