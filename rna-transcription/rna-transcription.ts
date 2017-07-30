6a72b78f29d688eaceb62f033fa100e0828d1a98
Paul Baker <paul.nelson.baker@gmail.com>
2017-07-30 00:56:53 -0600

    Simple implementation.

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -4,7 +4,21 @@ class Transcriptor {
   }
 
   toRna(dna: string): string {
-    return dna;
+    let rna = '';
+    for (let gene of dna) {
+      if (gene === 'G') {
+        rna += 'C';
+      } else if (gene === 'C') {
+        rna += 'G';
+      } else if (gene === 'T') {
+        rna += 'A';
+      } else if (gene === 'A') {
+        rna += 'U';
+      } else {
+        throw 'Invalid input DNA.';
+      }
+    }
+    return rna;
   }
 }
 
